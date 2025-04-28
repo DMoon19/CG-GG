37e2beac8422f33acd13e230041058566259e307
jagari5546 <jagari5546@gmail.com>
2025-04-28 07:41:46 -0500

    Lighting Gaviria

---
+++ Assets/Lectures/5_Lighting_Gaviria/HLSL/Lighting.hlsl
@@ -0,0 +1,25 @@
+#ifndef LIGHTING_CUSTOM_INCLUDED
+#define LIGHTING_CUSTOM_INCLUDED
+
+//#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+void GetMainLight_float(float3 PositionWS, out half3 Color, out float3 Direction, out float ShadowAttenuation)
+{
+    #if defined (SHADERGRAPH_PREVIEW)
+    Color = 1;
+    Direction = normalize(float3(1, 1, -1));
+    ShadowAttenuation = 1;
+    #else
+    float4 shadowCord = TransformWorldToShadowCoord(PositionWS);
+    Light light = GetMainLight();
+    Color = light.color;
+    Direction = light.direction;
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowIntensity = GetMainLightShadowStrength();
+
+    ShadowAttenuation = SampleShadowmap(shadowCord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowIntensity, false);
+    #endif
+    
+}
+
+#endif
